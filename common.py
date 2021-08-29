b014f1b50ddce81252fae97c4fde5a641f282d81
Jacksen Narvaez <janarvaez@unal.edu.co>
2021-08-28 22:13:30 -0500

    Add PERL method to evolve the system
    Adapt the ODE of the system, now it calculates the acceleration of each one of three bodies explicitly.
    Add a function to calculate the energy and angular momentum of the system. It's optional.
    Change from Verlet method to PEFRL method to the system's evolution.
    Adapt function to create images, now the masses leave a line over their journey.
    Solve the problem in the creation of video, when the libraries were importing.

--- common.py
+++ common.py
@@ -1,128 +1,159 @@
-# Supernova Code
-
 from copy import deepcopy
-from numpy import array, ones, empty, random, sqrt, pi
-from numpy.linalg import norm
+import numpy as np
+from numpy import linalg as LA
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from mpl_toolkits.mplot3d import Axes3D
 
+# Gravitational constant in units of [au^3 M_sun^-1 yr-2]
+G = 4*np.pi**2
 
-##### Simulation Parameters ########################################################
+#################################################---EXAMPLES---#################################################
+#Some examples of celestial bodies = [x, y, z, vx, vy, vz, mass, orbital period]
+Sun = np.array([0.,0.,0.,0.,0.,0.,1.,0.])
+Jupiter = np.array([3.733076999471E+00, 3.052424824299E+00, 1.217426663570E+00, -1.85782081, 2.00651219, 0.90532114, 9.54791e-04, 11.8618])
+Halley = np.array([0.33126099, -0.45385512, 0.16628889,-9.01382671, -7.04649889, -1.27585467,1.106378E-16,75.32])
+Geographos = np.array([-0.21765384, -0.77581474, -0.18955119, 7.66669988, -2.20533078, 0.22284989, 1.0E-17,1.39])
+a_2021PH27 = np.array([0.09174773, 0.09731134, 0.01034353, -14.54945423, 12.47980066, 11.64527326, 1.0e-5,0.31, .7886707956777496]) #Real approximate mass 1.0e-17
 
-# Gravitational constant in units of [au^3 M_sun^-1 yr-2]
-G = 4.*pi**2
-
-# Discrete time step.
-dt = 1.e-3 # [yr]
-
-####################################################################################
-
-class Body:
+def Acceleration(q0, mass):
     '''
-    Each Body object will represent a particle.
+    --------------------------------------------------
+    Acceleration(q0, mass):
+    Calculates the acceleration due to gravitation for 
+    each body in the 3 body system.
+    --------------------------------------------------
+    Arguments:
+    q0: numpy array with the initial condition data:
+        q0[0] = particle 1
+        q0[1] = particle 2
+        q0[2] = particle 3, where:
+        q0[i] = [x_i, y_i, z_i] for i=0,1,2
+    mass: masses of the particles
+        mass = [m1, m2, m3]
+    --------------------------------------------------
+    Returns:
+    a = NumPy array with the components of the 
+        acceleration
+        a[i] = [ax_i, ay_i, az_i] for i=0,1,2
     '''
-    def __init__(self, m, position, momentum):
-        '''
-        Creates a particle using the attributes
-        .mass : scalar
-        .position : NumPy array  with the coordinates [x,y,z]
-        .momentum : NumPy array  with the components [px,py,pz]
-        '''
-        self.m = m
-        self.m_pos = m * position
-        self.momentum = momentum
+    a = np.zeros([3, 3])
+    Deltaxyz = q0[0] - q0[1], q0[0] - q0[2], q0[1] - q0[2]    
+    r = LA.norm(Deltaxyz[0]), LA.norm(Deltaxyz[1]), LA.norm(Deltaxyz[2]) # Distance between particles
+    a[0,:] = -G * Deltaxyz[0] * mass[1] / r[0]**3 - G * Deltaxyz[1] * mass[2]/r[1]**3
+    a[1,:] = G * Deltaxyz[0] * mass[0]/r[0]**3 - G * Deltaxyz[2] * mass[2]/r[2]**3
+    a[2,:] = G * Deltaxyz[1] * mass[0]/r[1]**3 + G * Deltaxyz[2] * mass[1]/r[2]**3  
+    return a
+
+def Constants(q, mass):
+    '''
+    --------------------------------------------------
+    Constants(q, mass)
+    Calculates the total energy and total angular 
+    momentum of 3 particles interacting gravitationally.
+    --------------------------------------------------
+    Arguments:
+    q: Numpy array with the position and velocity of 
+        the particles with the format
+        q = [[x1, y1, z1, vx1, vy1, vz1],
+             [x2, y2, z2, vx2, vy2, vz2],
+             [x3, y3, z3, vx3, vy3, vz3]]
+    mass: NumPy array with the masses of the particles.
+        m = [m1, m2, m3]
+    --------------------------------------------------
+    Returns:
+    E = Total energy of the system
+    L = Magnitude of the total angular momentum the 
+        system
+    --------------------------------------------------
+    '''
+    speed2 = np.array(np.sum(q[:,3:]**2, axis=1))
+    r = LA.norm(q[0,:3] - q[1,:3]), LA.norm(q[0,:3] - q[2,:3]), LA.norm(q[1,:3] - q[2,:3])
+    U = np.array([mass[0]*mass[1]/r[0], mass[0]*mass[2]/r[1], mass[1]*mass[2]/r [2]])
+    E = np.sum(0.5*speed2*mass-2*G*U) # Total energy
+    L = np.cross(q[0,:3],(mass[0]*q[0,3:]))+ np.cross(q[1,:3],(mass[1]*q[1,3:]))\
+        + np.cross(q[2,:3],(mass[2]*q[2,3:]))#Total angular momentum vector
     
-    def position(self):
-        '''
-        Returns the physical coordinates of the Body.
-        '''
-        return self.m_pos / self.m
+    return E, LA.norm(L)
 
-
-def distance_between(body1, body2):
+def PEFRL(ODE, q0, mass, n, t):
     '''
-    Returns the distance between body1 and body2.
+    --------------------------------------------------
+    PEFRL(ODE, q0, mass, n, t)
+    --------------------------------------------------
+    Position Extended Forest-Ruth Like (PEFRL) method 
+    for time evolution.
+    Arguments:
+    ODE: function defining the system of ODEs
+    q0: numpy array with the initial values of
+        the functions in the ODEs system
+        q[0] : [x1, y1, z1, vx1, vy1, vz1]
+        q[1] : [x2, y2, z2, vx1, vy2, vz2]
+    mass: masses of the particles
+        mass = [m1, m2]
+    n:  Number of steps in the grid
+    t: array (t0,tf) whit the initial and 
+       final time.
+    --------------------------------------------------
+    Returns:
+    q = NumPy array with the components of the 
+        solution since t0 to tf
     '''
-    return norm(body1.position() - body2.position())
+    # Arrays to store the solution
+    q = np.zeros([n, 3, 6])  
+    q[0] = q0
+    
+    #stepsize for the iteration
+    h = (t[1]-t[0])/n
+    
+    #Parameter
+    xi = 0.1786178958448091E+00
+    gamma = -0.2123418310626054E+00
+    chi = -0.6626458266981849E-01 
+    
+    # Main Loop
+    for i in range(n-1):
+        x_1 = q[i,:,:3] + xi*h*q[i,:,3:]
+        F = ODE(x_1, mass)
+        v_1 = q[i,:,3:] + 0.5*(1.-2*gamma)*h*F
+        x_2 = x_1 + chi*h*v_1
+        F = ODE(x_2, mass)
+        v_2 = v_1 + gamma*h*F
+        x_3 = x_2 + (1.-2*(chi+xi))*h*v_2
+        F = ODE(x_3, mass)
+        v_3 = v_2 + gamma*h*F
+        x_4 = x_3 + chi*h*v_3
+        F = ODE(x_4, mass)
+        q[i+1,:,3:] = v_3 + 0.5*(1.-2*gamma)*h*F
+        q[i+1,:,:3] = x_4 + xi*h*q[i+1,:,3:]
 
-
-def gravitational_force(body1, body2):
+    return q     
+            
+def create_images(evolution, dt, center, img_step, image_folder='images/', video_name='my_video.mp4'):
     '''
-    Returns the gravitational force that body2 exerts on body1.
-    A short distance cutoff is introduced in order to avoid numerical
-    divergences in the gravitational force.
+    This function evolves the system in time using the PEFRL algorithm
     '''
-    cutoff_dist = 2.e-4
-    d = distance_between(body1, body2)
-    if d < cutoff_dist:
-        #print('Collision!')
-        # Returns no Force
-        return array([0., 0., 0.])
-    else:
-        # Gravitational force
-        return -G*body1.m*body2.m*(body1.position() - body2.position())/d**3
-
-
-def force_on(particle, other_bodies):
-    '''
-    Returns the total force on the particle due to the other_bodies
-    '''
-    return sum(gravitational_force(particle, body) for body in other_bodies)
-
-
-def verlet(particles, other_bodies, dt):
-    '''
-    Verlet method for time evolution.
-    '''
-    for p in particles:
-        force = force_on(p, other_bodies)
-        p.momentum += force*dt
-        p.m_pos += p.momentum*dt
-
-
-def system_init(masses, ini_positions, ini_momenta):
-    '''
-    This function initialize the N-body system by creating the objects of the Body class
-    '''
-    bodies = []
-    for i in range(len(masses)):
-        bodies.append(Body(masses[i], ini_positions[i], ini_momenta[i]))
-    return bodies
-
-
-
-def evolve(particles, n, center, plot_limit, img_step, image_folder='images/', video_name='my_video.mp4'):
-    '''
-    This function evolves the system in time using the Verlet algorithm
-    '''
+    
     # Limits for the axes in the plot
-    axis_limit = 1.5*plot_limit
-    lim_inf = [center[0]-axis_limit, center[1]-axis_limit, center[2]-axis_limit]
-    lim_sup = [center[0]+axis_limit, center[1]+axis_limit, center[2]+axis_limit]
-    # Principal loop over time iterations.
-    for i in range(n+1):
-        for p in particles:
-            verlet(particles, particles, dt)
-        #Write the image files
-        if i%img_step==0:
-            print("Writing image at iteration {0}".format(i))
-            plot_bodies(particles, i//img_step, 2*i*dt, lim_inf, lim_sup, image_folder)
-
+    boundary = 1.1*max(abs(np.amin(evolution[:,:,:3])),abs(np.amax(evolution[:,:,:3])))
+    lim_inf = [center[0]-boundary, center[1]-boundary, center[2]-boundary]
+    lim_sup = [center[0]+boundary, center[1]+boundary, center[2]+boundary]
+    
+    #Write the image files
+    for i in range(img_step,len(evolution),img_step):       
+        print("Writing image at iteration {0}".format(i))
+        plot_bodies(evolution[:i], i//img_step, 2*i*dt, lim_inf, lim_sup, image_folder)
 
 def plot_bodies(bodies, i, time, lim_inf, lim_sup, image_folder='images/'):
     '''
     Writes an image file with the current position of the bodies
     '''
     plt.rcParams['grid.color'] = 'dimgray'
-    #plt.rcParams['axes.edgecolor'] = 'dimgray'
-    #plt.rcParams['axes.labelcolor'] = 'dimgray'
     fig = plt.figure(figsize=(10,10), facecolor='black')
     ax = plt.gcf().add_subplot(111, projection='3d')
     ax.set_xlim([lim_inf[0], lim_sup[0]])
     ax.set_ylim([lim_inf[1], lim_sup[1]])
     ax.set_zlim([lim_inf[2], lim_sup[2]])
-    #ax.set_proj_type('ortho')
     ax.set_facecolor('black')
     ax.xaxis.pane.fill = False
     ax.yaxis.pane.fill = False
@@ -130,25 +161,20 @@ def plot_bodies(bodies, i, time, lim_inf, lim_sup, image_folder='images/'):
     ax.xaxis.pane.set_edgecolor('dimgray')
     ax.yaxis.pane.set_edgecolor('dimgray')
     ax.zaxis.pane.set_edgecolor('dimgray')
-    #ax.xaxis.label.set_color('dimgray')
-    #ax.yaxis.label.set_color('dimgray')
-    #ax.zaxis.label.set_color('dimgray')
-    #ax.set_xlabel('x')
-    #ax.set_ylabel('y')
-    #ax.set_zlabel('z')
-    #ax.grid(False)
-    for body in bodies:
-        pos = body.position()
-        ax.scatter(pos[0], pos[1], pos[2], marker='o', color='lightcyan')
+    colors=['gold','darkorange','cyan']
+    for j in range(3):
+        ax.plot(bodies[:,j,0], bodies[:,j,1],bodies[:,j,2])#,'-', color=colors[j])
+        ax.scatter(bodies[-1,j,0], bodies[-1,j,1], bodies[-1,j,2], marker='o', color=colors[j])
     ax.set_title(' Time : {:.3f} years'.format(time), color='dimgray')
     plt.gcf().savefig(image_folder+'image_{0:06}.png'.format(i))
     plt.close()
-
 
 def create_video(image_folder='images/', video_name='my_video.mp4'):
     '''
     Creates a .mp4 video using the stored files images
     '''
+    import os
+    os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
     from os import listdir
     import moviepy.video.io.ImageSequenceClip
     fps = 15
@@ -156,32 +182,44 @@ def create_video(image_folder='images/', video_name='my_video.mp4'):
     clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
     clip.write_videofile(video_name)
 
-
-
-
 if __name__=="__main__":
     '''
-    Example of 2-Body system: Sun-Earth
+    Example of 3-Body system: Sun-Jupiter and Body_3
     '''
-    # Bodies initial data
-    sunM = 1.           # Solar masses
-    earthM = 3.00273e-6 # Solar masses
-    sun_ini_pos = array([0., 0., 0.])              # Initial position of the Sun [au]
-    earth_ini_pos = array([1., 0., 0.])         # Initial position of the Earth [au]
-    sun_ini_momentum = array([0., 0., 0.])         # Initial Momentum of the Sun [au/yr]
-    earth_ini_momentum = array([0.,1.898986e-5,0.]) # Initial Momentum of the Earth [au/yr]
+    #Planets information
+    Body_1 = Sun # [x, y, z, vx, vy, vz, mass, orbital period]
+    name1 = 'Sun'
+    Body_2 = Jupiter # [x, y, z, vx, vy, vz, mass, orbital period]
+    name2 = 'Jupiter'
+    Body_3 = a_2021PH27 # [x, y, z, vx, vy, vz, mass, orbital period]
+    name3 = 'a_2021PH27'
     
-    masses = array([sunM, earthM])
-    ini_positions = array([sun_ini_pos, earth_ini_pos])
-    ini_momenta = array([sun_ini_momentum, earth_ini_momentum])
     
-    # Center of the image
-    center = array([0., 0., 0.])
-    # Limit for the plot
-    plot_limit = 2. # [au]
+    # Creation of the time grid (in years)
+    t = np.array([0.,max(Body_1[-1],Body_2[-1], Body_3[-1])])
     
-    # Number of time-iterations executed by the program.
-    n = 5000 # Time steps
+    # Number of steps in the grid
+    n = 10000
+    
+    # Arrays to store time information
+    t1 = np.linspace(t[0], t[1], n) 
+    dt = (t[1]-t[0])/n
+    print(f'dt={dt} and tf={t1[1]}')
+    # Mass
+    m1 = Body_1[6]
+    m2 = Body_2[6]
+    m3 = Body_3[6]
+    mass = np.array([m1, m2, m3])
+    
+    # Initial Conditions
+    q = np.zeros([n,3,6]) #  Motion information FR
+    q[0, 0] = Body_1[:6] # initial x, y, vx, vy to particle 1
+    q[0, 1] = Body_2[:6] # initial x, y, vx, vy to particle 2
+    q[0, 2] = Body_3[:6] # initial x, y, vx, vy to particle 3
+
+
+    # Solution to the problem using LeapFrog method
+    q = PEFRL(Acceleration, q[0], mass, n, t)
     
     # Frequency at which .PNG images are written.
     img_step = 50
@@ -189,7 +227,8 @@ if __name__=="__main__":
     image_folder = 'images/'
     # Name of the generated video
     video_name = 'my_video.mp4'
-    
-    bodies = system_init(masses, ini_positions, ini_momenta)
-    evolve(bodies, n, center, plot_limit, img_step, image_folder, video_name)
+    # Center of the image
+    center = np.array([0., 0., 0.])
+
+    create_images(q, dt, center, img_step, image_folder, video_name)
     create_video(image_folder, video_name)
