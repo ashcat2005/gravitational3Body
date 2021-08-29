736bd3d53a0b8a4c6a5aad5f45aa80e7ecdaad9d
Jacksen Narvaez <janarvaez@unal.edu.co>
2021-08-28 22:25:46 -0500

    Add values from test systems and evaluate the evolution of orbital elements

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
 from common import *
+from conversion import *
+from Plots import *
 
-######### MAIN PROGRAM ########################################################
+from numpy import array, zeros, linspace
 
+#Bodies data as [x, y, z, vx, vy, vz, mass, orbital period, Apsis]
 
-'''
-Example of 2-Body system
-'''
+Sun = array([0.,0.,0.,0.,0.,0.,1.,0., 0.])
+Jupiter = array([-2.11654703, 4.47630812, 0.0287253, -2.61291102,-1.23587974, 0.06368842, 1.0e-01, 12., 5.46])
+Hidalgo = array([0.56084979, 1.5044718, 1.09763743,-5.37114217, 0.42400368, 2.16328483, 1.0E-17,13.72, 9.519253976700893])
+a_2021PH27 = array([0.09174773, 0.09731134, 0.01034353, -14.54945423, 12.47980066, 11.64527326, 1.0e-5,0.31, .7886707956777496]) #Real approximate mass 1.0e-17
 # Bodies initial data
-sunM = 1.           # Solar masses
-earthM = 3.00273e-6 # Solar masses
-sun_ini_pos = array([0., 0., 0.])              # Initial position of the Sun
-earth_ini_pos = array([1., 0., 0.])         # Initial position of the Earth
-sun_ini_momentum = array([0., 0., 0.])         # Initial Momentum of the Sun
-earth_ini_momentum = array([0.,1.898986e-5,0.]) # Initial Momentum of the Earth
+Body_1 = Sun # Body 1
+Body_2 = Jupiter # Body 2
+Body_3 = a_2021PH27  # Body 3
+names = ['Sun', 'Jupiter', '2021PH27']
 
-masses = array([sunM, earthM])
-ini_positions = array([sun_ini_pos, earth_ini_pos])
-ini_momenta = array([sun_ini_momentum, earth_ini_momentum])
-    
-# Center of the image
-center = array([0., 0., 0.])
-# Limit for the plot
-plot_limit = 2. #kpc
-    
+# Time's interval [yr]
+t = array([0.,max(Body_1[7],Body_2[7], Body_3[7])])
+
 # Number of time-iterations executed by the program.
-n = 5000 # Time steps
+n = 100000 # Time steps
+
+# Discrete time step. 
+dt = (t[1]-t[0])/n # [yr]
+print(f'dt={dt} and tf={t[1]}')
+
+# Array to store time information
+t1 = linspace(t[0], t[1], n)
     
+# Masses in Solar masses
+m1 = Body_1[6]
+m2 = Body_2[6]
+m3 = Body_3[6]
+masses = array([m1, m2, m3])
+
+#Array of Energy and Total angular momentum of the system
+Consts= zeros([n+1,2])
+
+# Initial Conditions
+Evolution = zeros([n,3,6]) #  Motion information FR
+Evolution[0, 0] = Body_1[:6] # initial x, y, z, vx, vy to particle 1
+Evolution[0, 1] = Body_2[:6] # initial x, y, z, vx, vy to particle 2
+Evolution[0, 2] = Body_3[:6] # initial x, y, z, vx, vy to particle 3
+
+
+# Solution to the problem using LeapFrog method
+print('Evolution in process...')
+Evolution = PEFRL(Acceleration, Evolution[0], masses, n, t)
+print('The process has finished.')
+
+Plot_orbit(Evolution, names)
 # Frequency at which .PNG images are written.
 img_step = 50
 # Folder to save the images
 image_folder = 'images/'
 # Name of the generated video
-video_name = 'Sun-Earth.mp4'
-    
-bodies = system_init(masses, ini_positions, ini_momenta)
-evolve(bodies, n, center, plot_limit, img_step, image_folder, video_name)
-create_video(image_folder, video_name)
+video_name = names[0]+'-'+names[1]+'-'+names[2]+'.mp4'
+# Center of the image
+center = array([0., 0., 0.])
 
+#Generate video
+#create_images(q, dt, center, img_step, image_folder, video_name)
+#create_video(image_folder, video_name)
+
+#Orbital parameters
+print('Transforming to orbital elements...')
+Orbital_elements = zeros([n,2,5])
+for i in range(n):
+    Orbital_elements[i,0]=coords_to_op(G*m1,Evolution[i,1,:3]-Evolution[i,0,:3] , Evolution[i,1,3:]-Evolution[i,0,3:])
+    Orbital_elements[i,1]=coords_to_op(G*m1,Evolution[i,2,:3]-Evolution[i,0,:3] , Evolution[i,2,3:]-Evolution[i,0,3:])
+print('Ok!')
+
+Plot_OrbElem(t1,Orbital_elements[:,1])
 
